a2229e5604914d1f12760a159ed858da6edff3fe
Gytis Užkuraitis <gytis.uzkuraitis@gmail.com>
2024-03-03 22:39:15 +0200

    added reservation method

---
+++ Restoranas/Models/IndexModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Restoranas.Models
+{
+    public class IndexModel
+    {
+        public List<TableInfo> Tables { get; set; }
+        public string SelectedDate { get; set; } // Papildoma savybė pasirinktai datai
+
+    }
+}
---
+++ Restoranas/Models/TableInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Restoranas.Models
+{
+    public class TableInfo
+    {
+        public string TableNumber { get; set; }
+        public int Capacity { get; set; }
+    }
+}
