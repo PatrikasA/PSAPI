04c3eae021a86c12b13d598468936f466493b211
PatrikasA <45782374+PatrikasA@users.noreply.github.com>
2024-03-18 00:33:37 +0200

    Added login and register

---
+++ Restoranas/Controllers/UserController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using Restoranas.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Restoranas.Helper;
+
+namespace Restoranas.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly string connString = "Host=ep-solitary-forest-a28gt5ec-pooler.eu-central-1.aws.neon.tech;Port=5432;Database=psapi_faxai;Username=psapi_faxai_owner;Password=g3xbiOmuETp7;";
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(User userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var conn = new NpgsqlConnection(connString))
+                {
+                    try
+                    {
+                        conn.Open();
+                        var query = "SELECT * FROM naudotojas WHERE prisijungimo_vardas = @prisijungimoVardas AND slaptazodis = @slaptazodis";
+                        using (var cmd = new NpgsqlCommand(query, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@prisijungimoVardas", userModel.prisijungimo_vardas);
+                            cmd.Parameters.AddWithValue("@slaptazodis", userModel.slaptazodis);
+
+                            using (var reader = cmd.ExecuteReader())
+                            {
+                                if (reader.Read())
+                                {
+                                    return RedirectToAction("Aktualiausi", "Apsilankymai");
+                                }
+                                else
+                                {
+                                    ModelState.AddModelError("", "Invalid login attempt.");
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError("", $"Database error: {ex.Message}");
+                    }
+                }
+            }
+
+            return View(userModel);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(User userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                userModel.naudotojo_tipas_id = 1;
+                using (var conn = new NpgsqlConnection(connString))
+                {
+                    try
+                    {
+                        conn.Open();
+                        var query = "INSERT INTO naudotojas (prisijungimo_vardas, slaptazodis, naudotojo_tipas_id) VALUES (@prisijungimoVardas, @slaptazodis, @naudotojoTipasId)";
+                        using (var cmd = new NpgsqlCommand(query, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@prisijungimoVardas", userModel.prisijungimo_vardas);
+                            cmd.Parameters.AddWithValue("@slaptazodis", userModel.slaptazodis);
+                            cmd.Parameters.AddWithValue("@naudotojoTipasId", userModel.naudotojo_tipas_id);
+
+                            var result = cmd.ExecuteNonQuery();
+                            if (result > 0)
+                            {
+                                return RedirectToAction("Login");
+                            }
+                            else
+                            {
+                                ModelState.AddModelError("", "Registration failed due to a database error.");
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError("", $"An error occurred: {ex.Message}");
+                    }
+                }
+            }
+
+            return View(); 
+        }
+    }
+}
---
+++ Restoranas/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Restoranas.Models
+{
+    public class User
+    {
+        public int naudotojo_id {  get; set; }
+        public string prisijungimo_vardas { get; set; }
+        public string slaptazodis { get; set; }
+        public int naudotojo_tipas_id {  get; set; }
+    }
+}
