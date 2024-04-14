9fbdc0c0d1ec4870423847af2efeba0a5469a263
PatrikasA <45782374+PatrikasA@users.noreply.github.com>
2024-04-14 16:13:43 +0300

    Visits and waiter controller

---
+++ Restoranas/Controllers/WaiterController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using Restoranas.Models;
+
+namespace Restoranas.Controllers
+{
+    public class WaiterController : Controller
+    {
+        private readonly string connString = "Host=ep-solitary-forest-a28gt5ec-pooler.eu-central-1.aws.neon.tech;Port=5432;Database=psapi_faxai;Username=psapi_faxai_owner;Password=g3xbiOmuETp7;";
+
+
+        [HttpGet]
+        public IActionResult MainViewWaiter()
+        {
+            return View("MainViewWaiter");
+        }
+
+
+        [HttpGet]
+        public IActionResult VisitsPageWaiter()
+        {
+            var visits = new List<Visit>();
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+                var query = "SELECT * FROM apsilankymas WHERE uzbaigtas = false";
+                using (var cmd = new NpgsqlCommand(query, conn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            visits.Add(new Visit
+                            {
+                                apsilankymo_id = reader.GetInt32(reader.GetOrdinal("apsilankymo_id")),
+                                data = reader.GetDateTime(reader.GetOrdinal("data")),
+                                zmoniu_skaicius = reader.GetInt32(reader.GetOrdinal("zmoniu_skaicius")),
+                                apmoketas = reader.GetBoolean(reader.GetOrdinal("apmoketas")),
+                                naudotojo_id = reader.GetInt32(reader.GetOrdinal("naudotojo_id")),
+                                staliuko_nr = reader.GetInt32(reader.GetOrdinal("staliuko_nr")),
+                                uzbaigtas = reader.GetBoolean(reader.GetOrdinal("uzbaigtas"))
+                            });
+                        }
+                    }
+                }
+            }
+            return View(visits);
+        }
+
+        [HttpGet]
+        public IActionResult CreateVisit()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CancelVisitConfirmation(int id)
+        {
+            ViewBag.VisitId = id;
+            return View();
+        }
+    }
+}
---
+++ Restoranas/Models/Visit.cs
@@ -0,0 +1,13 @@
+﻿namespace Restoranas.Models
+{
+    public class Visit
+    {
+        public int apsilankymo_id { get; set; }
+        public DateTime data { get; set; }
+        public int zmoniu_skaicius { get; set; }
+        public bool apmoketas { get; set; }
+        public int naudotojo_id { get; set; }
+        public int staliuko_nr { get; set; }
+        public bool uzbaigtas { get; set; }
+    }
+}
